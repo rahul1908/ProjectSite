e5e7242dead5007268b07b3c606202ad3c81f29f
Rahul Nepaulawa <222002425@stu.ukzn.ac.za>
2024-10-14 09:11:40 +0200

    Add project

---
+++ Restricted/AddProject.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Title="Add Project" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AddProject.aspx.cs" Inherits="ProjectSite.Restricted.AddProject" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+    <!-- You can add additional head content here if needed -->
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Enter Project Details</h2>
+    
+    <!-- ASP.NET Form for project details -->
+    <asp:Panel runat="server">
+        <!-- Client Dropdown -->
+        <div>
+            <label for="ddlClient">Client:</label>
+            <asp:DropDownList ID="ddlClient" runat="server" DataTextField="ClientName" DataValueField="Client_ID">
+            </asp:DropDownList>
+        </div>
+        <br />
+
+        <!-- Project Name -->
+        <div>
+            <label for="txtProjectName">Project Name:</label>
+            <asp:TextBox ID="txtProjectName" runat="server"></asp:TextBox>
+        </div>
+        <br />
+
+        <!-- Project Description -->
+        <div>
+            <label for="txtProjectDescription">Project Description:</label>
+            <asp:TextBox ID="txtProjectDescription" runat="server"></asp:TextBox>
+        </div>
+        <br />
+
+        <!-- Project Start Date -->
+        <div>
+            <label for="txtStartDate">Project Start Date:</label>
+            <asp:TextBox ID="txtStartDate" runat="server" TextMode="Date"></asp:TextBox>
+        </div>
+        <br />
+
+        <!-- Project End Date -->
+        <div>
+            <label for="txtEndDate">Project End Date:</label>
+            <asp:TextBox ID="txtEndDate" runat="server" TextMode="Date"></asp:TextBox>
+        </div>
+        <br />
+
+        <!-- Project Budget -->
+        <div>
+            <label for="txtBudget">Project Budget:</label>
+            <asp:TextBox ID="txtBudget" runat="server"></asp:TextBox>
+        </div>
+        <br />
+
+        <!-- Manager ID -->
+        <div>
+            <label for="txtManagerID">Manager ID:</label>
+            <asp:TextBox ID="txtManagerID" runat="server"></asp:TextBox>
+        </div>
+        <br />
+
+        <!-- Submit Button -->
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+    </asp:Panel>
+</asp:Content>
+
---
+++ Restricted/AddProject.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ProjectSite.Restricted
+{
+    public partial class AddProject : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadClients();
+            }
+        }
+
+        // Load clients into dropdown
+        protected void LoadClients()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["Server=146.230.177.46;Database=G8Wst2024;User Id=G8Wst2024;Password=09ujd"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string query = "SELECT Client_ID, ClientName FROM Clienttbl"; // Adjust this query to match your client table
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                ddlClient.DataSource = cmd.ExecuteReader();
+                ddlClient.DataTextField = "Client_Name";
+                ddlClient.DataValueField = "Client_ID";
+                ddlClient.DataBind();
+            }
+        }
+
+        // Handle form submission
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string connString = ConfigurationManager.ConnectionStrings["Server=146.230.177.46;Database=G8Wst2024;User Id=G8Wst2024;Password=09ujd"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                string query = @"INSERT INTO Projects 
+                                (Client_ID, Project_Name, Project_Description, Project_Start_date, Project_End_date, Project_Budget, Manager_ID) 
+                                VALUES 
+                                (@Client_ID, @Project_Name, @Project_Description, @Project_Start_date, @Project_End_date, @Project_Budget, @Manager_ID)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Client_ID", ddlClient.SelectedValue);
+                cmd.Parameters.AddWithValue("@Project_Name", txtProjectName.Text);
+                cmd.Parameters.AddWithValue("@Project_Description", txtProjectDescription.Text);
+                cmd.Parameters.AddWithValue("@Project_Start_date", txtStartDate.Text);
+                cmd.Parameters.AddWithValue("@Project_End_date", txtEndDate.Text);
+                cmd.Parameters.AddWithValue("@Project_Budget", txtBudget.Text);
+                cmd.Parameters.AddWithValue("@Manager_ID", txtManagerID.Text);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            // Redirect or show a success message
+            Response.Write("Project inserted successfully!");
+        }
+    }
+}
+        
+    
